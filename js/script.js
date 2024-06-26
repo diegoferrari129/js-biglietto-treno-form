96fa50205165e0ea871d70a229212f943e86de4b
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-26 18:22:09 +0200

    [UPD] added the calc of the base price  and total price to script.js

--- js/script.js
+++ js/script.js
@@ -5,5 +5,21 @@ button.addEventListener('click', function() {
 
     // CHIEDI ALL'UTENTE L'ETà DEL PASSEGGERO
     let age = document.getElementById('age').value;
+
+    // CALCOLA IL PREZZO DEL BIGLIETTO IN BASE ALLA DISTANZA IN KM.
+    let base_price = distance * 0.21;
+
+    // APPLICA UNO SCONTO DEL 20% PER I MINORENNI E DEL 40% PER GLI OVER 65
+    let discount = 0;
+    if (age < 18) {
+        discount = base_price * 0.2
+    } 
+    else if (age > 65) {
+        discount = base_price * 0.4
+    }
+    let total_price = base_price - discount;
+
+    //MOSTRA IL RISULTATO NELLA CONSOLE DEL BROWSER
+    console.log(total_price);
 });
 
