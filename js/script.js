505959459a55a996297fdcb2f16fa3f4322b7f02
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-26 18:19:19 +0200

    [UPD] recall the calc button and input values to script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
+const button = document.getElementById('calc_total_price');
+button.addEventListener('click', function() {
+    // CHIEDI ALL'UTENTE LA DISTANZA IN KM.
+    let distance = document.getElementById('distance').value;
 
+    // CHIEDI ALL'UTENTE L'ETà DEL PASSEGGERO
+    let age = document.getElementById('age').value;
+});
 
