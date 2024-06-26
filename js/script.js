c56feb611b58b0f1ba5c383e8dc473576fcf96bd
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-26 23:43:58 +0200

    [UPD] added content to form

--- js/script.js
+++ js/script.js
@@ -5,16 +5,17 @@ button.addEventListener('click', function() {
 
     // CHIEDI ALL'UTENTE L'ETà DEL PASSEGGERO
     let age = document.getElementById('age').value;
-
+    let minorenne = document.getSelection('minorenne').value;
+    let old = document.getSelection('old').value;
     // CALCOLA IL PREZZO DEL BIGLIETTO IN BASE ALLA DISTANZA IN KM.
     let base_price = distance * 0.21;
 
     // APPLICA UNO SCONTO DEL 20% PER I MINORENNI E DEL 40% PER GLI OVER 65
     let discount = 0;
-    if (age < 18) {
+    if (age = minorenne) {
         discount = base_price * 0.2
     } 
-    else if (age > 65) {
+    else if (age = old) {
         discount = base_price * 0.4
     }
     let total_price = base_price - discount;
